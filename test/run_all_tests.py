8740bcd2ce7e34f391d8ca1de0c08cf49acbaa7c
schroederB <benjamin-schroeder@outlook.com>
2019-10-30 13:31:43 +0100

    optimized pip

--- test/run_all_tests.py
+++ test/run_all_tests.py
@@ -4,18 +4,15 @@ import unittest
 #import importlib
 
 if __name__ == "__main__":
-
     #include path:
     print("PWD", os.getenv("PWD"))
-
     raw_path = os.getenv("PWD")
-    print(raw_path.split("/Ensembler"))
     include_path = raw_path.split("/Ensembler")[0]
     sys.path.append(include_path)
-    print(include_path)
+    print("sys path append:",include_path)
 
     #FILE MANAGMENT
-    test_root_dir = os.path.dirname(__file__)
+    test_root_dir = raw_path
     print("TEST ROOT DIR: " + test_root_dir)
 
     ##gather all test_files
@@ -23,7 +20,8 @@ if __name__ == "__main__":
     for dir in os.walk(test_root_dir):
         test_files.extend([dir[0]+"/"+path for path in dir[2] if( path.startswith("test") and path.endswith(".py") and not "test_run_all_tests" in path)])
     if(len(test_files) == 0):
-        raise IOError("Could not find any test in : ", test_root_dir)
+        print("Could not find any test in : ", test_root_dir)
+        exit(1)
 
     ##get module import paths - there should be a function for that around
     modules = []
