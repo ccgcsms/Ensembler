aadae6db7c7ace2ec64860b16e0c1b98d07c68c7
schroederB <benjamin-schroeder@outlook.com>
2019-10-30 13:51:58 +0100

    optimized pip

--- test/run_all_tests.py
+++ test/run_all_tests.py
@@ -55,15 +55,14 @@ if __name__ == "__main__":
         print("TEST SUIT TESTS: ", suite.countTestCases())
         test_runner = unittest.TextTestRunner(verbosity=5)
         test_result = test_runner.run(suite)
-        print(vars(test_result))
-        print("SUMMARY: ")
+
+        print("\nSUMMARY: ")
         print("\ttests run: ", test_result.testsRun)
-        print("\ttest Failures: ", test_result.failures)
-        print("\ttest Errors: ", test_result.errors)
-
+        print()
         if(len(test_result.failures)>0 or len(test_result.errors)>0 ):
+            print("\ttest Failures: \n\t\t", "\n\t\t".join(test_result.failures))
+            print("\ttest Errors:  \n\t\t", "\n\t\t".join(test_result.errors))
             raise Exception("Found FAILURE")
-
         exit(0)
     except Exception as err:
         print("Test did not finish successfully!\n\t"+"\n\t".join(err.args))
